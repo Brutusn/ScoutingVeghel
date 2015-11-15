9042ff5878b98ae99718bcaae168555690dc8429
Colin-TUE <colin@lambrechts4u.nl>
2015-11-15 16:49:17 +0100

    Commented the debugging output

--- php/Reservering.php
+++ php/Reservering.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
+//ini_set('display_startup_errors',1);
+//ini_set('display_errors',1);
+//error_reporting(-1);
 
 require_once("DB2.php");
 
