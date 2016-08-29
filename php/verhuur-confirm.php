bf3474d508227293ba8a5d36dfc31fc83dc84ad9
Bart Niessen <bart.niessen@gmail.com>
2016-08-29 22:05:41 +0200

    Fixes #14 by adding some styling to the confirmation page

--- php/verhuur-confirm.php
+++ php/verhuur-confirm.php
@@ -35,14 +35,14 @@ if(isset($_GET["key"])) {
  * Shows the message that indicates the Verhuring is confirmed
  */
 function confirmAccept() {
-	redirect("Uw reservering is bevestigd, u hoort zo speodig mogelijk van ons.");
+	redirect("Uw reservering is bevestigd, u hoort zo spoedig mogelijk van ons.");
 }
 
 /**
  * Shows the message that indicates the Verhuring is already confirmed
  */
 function confirmAlready() {
-	redirect("Uw reservering is reeds bevestigd, u hoort zo speodig mogelijk van ons.");
+	redirect("Uw reservering is reeds bevestigd, u hoort zo spoedig mogelijk van ons.");
 }
 
 /**
@@ -53,15 +53,55 @@ function confirmDecline() {
 }
 
 function redirect($msg) {
-	echo ("<SCRIPT LANGUAGE='JavaScript'>
-			window.alert('$msg')
-           	window.location.href='http://nieuw.scoutingveghel.nl';
-       </SCRIPT>
-       <NOSCRIPT>
-       $msg
-       <br>
-           <a href='http://nieuw.scoutingveghel.nl'>Click here if you are not redirected.</a>
-       </NOSCRIPT>");
+	echo ('<!DOCTYPE html>
+			<html>
+				<head>
+					<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+					<meta name="no-robots" content="all">
+					<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+
+					<title>Bevestiging verhuur | Scouting Veghel</title>
+					<link href="../css/style.min.css" type="text/css" rel="stylesheet">
+
+					<link rel="apple-touch-icon" sizes="57x57" href="../images/favicons/apple-touch-icon-57x57.png">
+					<link rel="apple-touch-icon" sizes="60x60" href="../images/favicons/apple-touch-icon-60x60.png">
+					<link rel="apple-touch-icon" sizes="72x72" href="../images/favicons/apple-touch-icon-72x72.png">
+					<link rel="apple-touch-icon" sizes="76x76" href="../images/favicons/apple-touch-icon-76x76.png">
+					<link rel="apple-touch-icon" sizes="114x114" href="../images/favicons/apple-touch-icon-114x114.png">
+					<link rel="apple-touch-icon" sizes="120x120" href="../images/favicons/apple-touch-icon-120x120.png">
+					<link rel="apple-touch-icon" sizes="144x144" href="../images/favicons/apple-touch-icon-144x144.png">
+					<link rel="apple-touch-icon" sizes="152x152" href="../images/favicons/apple-touch-icon-152x152.png">
+					<link rel="apple-touch-icon" sizes="180x180" href="../images/favicons/apple-touch-icon-180x180.png">
+
+					<link rel="icon" type="image/png" href="../images/favicons/favicon-32x32.png" sizes="32x32">
+					<link rel="icon" type="image/png" href="../images/favicons/android-chrome-192x192.png" sizes="192x192">
+					<link rel="icon" type="image/png" href="../images/favicons/favicon-96x96.png" sizes="96x96">
+					<link rel="icon" type="image/png" href="../images/favicons/favicon-16x16.png" sizes="16x16">
+					<link rel="manifest" href="../images/favicons/manifest.json">
+					<link rel="mask-icon" href="../images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+					<link rel="shortcut icon" href="../images/favicons/favicon.ico">
+
+					<meta name="msapplication-TileColor" content="#da532c">
+					<meta name="msapplication-TileImage" content="../images/favicons/mstile-144x144.png">
+					<meta name="msapplication-config" content="../images/favicons/browserconfig.xml">
+
+					<meta name="theme-color" content="#dddac4">
+
+					<meta http-equiv="refresh" content="10; url=http://nieuw.scoutingveghel.nl/" />
+				</head>
+				<body>
+				<div class="activiteiten fixed-background" style="padding: 15vh 0 15vh 0; height: 100vh; color: #202020;">
+					<div class="section verhuur">
+					<span class="size-limit">
+						<h2>Status bevestiging genomen optie:</h2>
+						<p><strong>' . $msg . '</strong></p>
+						<a href="http://nieuw.scoutingveghel.nl" style="color: inherit; font-size: 75%;">Klik hier als u niet teruggestuurd wordt naar de begin pagina.</a>
+					</span>
+					</div>
+				</div>
+				</body>
+			</html>');
 	header('HTTP/1.1 200 Ok');
     exit;
 }
