b8d0979f8319ff642318d361ca8b31656f5c5b82
Colin-TUE <colin@lambrechts4u.nl>
2020-08-08 12:55:43 +0200

    Add poetser name to poetser mail.

--- cron/poetsrooster.php
+++ cron/poetsrooster.php
@@ -35,31 +35,48 @@ $POETS_TASKS = [
   12 => "Ramen wassen"
 ];
 
+$POETSERS = [
+  1 => "Website-Jan",
+  2 => "Website-Feb",
+  3 => "Website-Mar",
+  4 => "Website-Apr",
+  5 => "Website-Mei",
+  6 => "Website-Jun",
+  7 => "Website-Jul",
+  8 => "Website-Aug",
+  9 => "Website-Sep",
+  10 => "Website-Okt",
+  11 => "Website-Nov",
+  12 => "Website-Dec",
+];
+
 //Determine which month it is
 $now = new DateTime();
 $month = $now->format('n');
-//Determine based ont he month who to send an email and what the specific tasks is
+//Determine based on the month who to send an email and what the specific tasks is
 $mails = $POETS_ROOSTER[$month];
+$poetsers = $POETSERS[$month];
 $task = $POETS_TASKS[$month];
 
 //sent the email to all emails found
 $toMail = implode(", ", $mails);
-sendPoetsEmail($toMail, $task);
+sendPoetsEmail($toMail, $poetsers, $task);
 
 
 /**
 * Send an email to the poetsers of this month according to the POETSROOSTER
 *
-* @param $toMail The mail address to wich this email needs to be sendDocuments
+* @param $toMail The mail address to which this email needs to be sendDocuments
+* @param $poetsers The specific group name that needs to poets
 * @param $task The specific task for this month
 * @return void
 */
-function sendPoetsEmail($toMail, $task){
+function sendPoetsEmail($toMail, $poetsers, $task){
   global $POETS_CHECKLIST_URL;
   global $MAIL_ADDRESS_BEHEER;
   
   $subject = "Poetsers van de maand";
-  $message = htmlentities("Beste poetsers van de maand,\r\n
+  $message = htmlentities("Beste poetsers van de maand: ". $poetsers .",\r\n
   Hierbij een herrinnering dat jullie moeten poetsen deze maand. De taak die deze week extra op de planning staat is: " . $task . ".\r\n
   De checklist is te vinden op onze website: ". $POETS_CHECKLIST_URL ." \r\n
   Met vriendelijke groeten,\r\n
